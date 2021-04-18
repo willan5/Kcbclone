88948cd3fc67f78f173e44b28e1f1dc584f1e599
Willan <kwanjehia@gmail.com>
2021-04-18 03:49:05 -0700

    JS Commit

--- kcb.js
+++ kcb.js
@@ -1,6 +1,19 @@
 $(function(){
-    $('country').on('click', function(e){
-        $('this').parent().toggleClass('open');
-        e.preventDefault();});
+    $(document).mouseup(function (e){
+        var container = $(".country-selector");	
+        //var mega = $(".mega-menu, header .sub-nav .has-children");
+    
+        if (!container.is(e.target) && container.has(e.target).length === 0) 
+        {	
+            container.removeClass('open');
+        }
+        
+        /*if (!mega.is(e.target) && mega.has(e.target).length === 0) 
+        {	
+            $('header .sub-nav .has-children').removeClass('active');
+            $('header .mega-menu').removeClass('open');
+        }*/
+        
+    }); 
                     
 });
